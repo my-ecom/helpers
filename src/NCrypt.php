94580befeeea9756a719ba12e6f00d1b53435e79
unknown <hqnhatdn@gmail.com>
2022-09-09 16:17:34 +0700

    Update

--- src/NCrypt.php
+++ src/NCrypt.php
@@ -2,32 +2,26 @@
 namespace oangia;
 
 class NCrypt {
-    function __construct($secret_key, $cipher_algo = 'AES-128-CBC')
-    {
-        $this->secret_key = $secret_key;
-        $this->cipher_algo = $cipher_algo;
-    }
-
-    public function encrypt($data)
+    public static function encrypt($data, $secret_key)
     {
         $plaintext = $data;
-        $ivlen = openssl_cipher_iv_length($cipher = $this->cipher_algo);
+        $ivlen = openssl_cipher_iv_length($cipher = 'AES-128-CBC');
         $iv = openssl_random_pseudo_bytes($ivlen);
-        $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $this->secret_key, $options = OPENSSL_RAW_DATA, $iv);
-        $hmac = hash_hmac('sha256', $ciphertext_raw, $this->secret_key, $as_binary = true);
+        $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $secret_key, $options = OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $secret_key, $as_binary = true);
         $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
         return $ciphertext;
     }
 
-    public function decrypt($data)
+    public static function decrypt($data, $secret_key)
     {
         $c = base64_decode($data);
-        $ivlen = openssl_cipher_iv_length($cipher = $this->cipher_algo);
+        $ivlen = openssl_cipher_iv_length($cipher = 'AES-128-CBC');
         $iv = substr($c, 0, $ivlen);
         $hmac = substr($c, $ivlen, $sha2len = 32);
         $ciphertext_raw = substr($c, $ivlen + $sha2len);
-        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $this->secret_key, $options = OPENSSL_RAW_DATA, $iv);
-        $calcmac = hash_hmac('sha256', $ciphertext_raw, $this->secret_key, $as_binary = true);
+        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $secret_key, $options = OPENSSL_RAW_DATA, $iv);
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $secret_key, $as_binary = true);
         if (hash_equals($hmac, $calcmac))
         {
             return $original_plaintext;
